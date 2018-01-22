2f893d17dbc3925a6e43043be91bfb7e81fe3ef6
Do Hoerin <hellodhlyn@gmail.com>
2018-01-22 23:00:58 +0900

    로그인 기능 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,7 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    'airbnb-base'
   ],
   // required to lint *.vue files
   plugins: [
@@ -24,6 +23,8 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
   }
 }
---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="login">
+    <h1>갤러리에 로그인</h1>
+    <form>
+      <input name="password" type="password" placeholder="비밀번호" v-model="form.password">
+      <button @click.prevent="submit">로그인</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { auth } from '../../lib/lynlab';
+
+export default {
+  name: 'Login',
+  methods: {
+    submit() {
+      auth(this.form.password)
+        .then((result) => {
+          if (result) {
+            this.$router.push('/');
+          } else {
+            alert('비밀번호가 틀립니다.'); // eslint-disable-line no-alert
+          }
+        });
+    },
+  },
+  data() {
+    return {
+      form: {},
+    };
+  },
+};
+</script>
